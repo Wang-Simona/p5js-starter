89d89658e57d93a88c1a539f0e7b60381429accb
Wang-Simona <wyingxuan9@gmail.com>
2025-05-06 10:37:07 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,15 +1,38 @@
+let font;
+
+function preload() {
+  font = loadFont("迷你简雪君.TTF");
+}
+
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(windowWidth, windowHeight, "webgl");
 }
 
 function draw() {
-  background(220);
-  const s = 100;
+  background("white");
+  orbitControl();
 
-  rect(0, 0, s, s);
-  rect(0, height - s, s, s);
-  rect(width - s, 0, s, s);
-  rect(width - s, height - s, s, s);
+  stroke("blue");
+  strokeWeight(4);
+  line(0, 0, width, 0);
+
+  stroke("#DC143C");
+  strokeWeight(4);
+  line(0, 0, 0, height);
+
+  stroke("red");
+  strokeWeight(4);
+  box(100);
+
+  push();
+  translate(width, 0);
+  box(100);
+  pop();
+
+  push();
+  translate(0, height);
+  box(100);
+  pop();
 }
 
 function windowResized() {
