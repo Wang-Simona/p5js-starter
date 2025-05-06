338b669f88017ed3d993dcf892443c6f34630b29
Wang-Simona <wyingxuan9@gmail.com>
2025-05-06 09:38:55 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -6,7 +6,7 @@ function draw() {
   background(220);
   const s = 100;
 
-  rect(0, 0, s, s);
+  rect(1, 1, s, s);
   rect(0, height - s, s, s);
   rect(width - s, 0, s, s);
   rect(width - s, height - s, s, s);
